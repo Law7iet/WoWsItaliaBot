60f5072191e40dff42279d4c8616c255b33b834c
Han Chu <leonardo.chu@icloud.com>
2022-12-21 18:26:58 +0100

    Update mongo API

--- api/mongo/README.md
+++ api/mongo/README.md
@@ -5,118 +5,135 @@ A simple version of MongoDB api for WoWsItalia.
 ## Database `WoWsItaliaDB`
 
 ### Collection `clans`
-```json
+```json lines
 {
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "$id": "schemas/clans/detail",
-  "title": "Clan Data",
-  "description": "This document records the details of a clan",
-  "type": "object",
-  "properties": {
-    "_id": {
-      "description": "A unique identifier for MongoDB (ObjectId)",
-      "type": "string"
-    },
-    "id": {
-      "description": "The WoWs' identifier of the clan",
-      "type": "number"
-    },
-    "name": {
-      "description": "The name of the clan",
-      "type": "string"
-    },
-    "tag": {
-      "description": "The tag of the clan",
-      "type": "string",
-      "maxLength": 5
-    },
-    "representation": {
-      "description": "The Discord identifiers of the clan's representations",
-      "type": "array",
-      "minItems": 0,
-      "maxItems": 2,
-      "items": {
-        "type": "number"
-      }
-    },
-    "rank": {
-      "description": "The clan battle's data of the clan",
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "season": {
-            "description": "The season of clan battle",
-            "type": "number"
-          },
-          "score": {
-            "description": "The scores obtained during the specific season of clan battle",
-            "type": "array",
-            "items": {
-              "type": "object",
-              "properties": {
-                "date": {
-                  "description": "The score of the clan in the end of the day",
-                  "type": "string",
-                  "format": "date"
-                },
-                "alpha": {
-                  "description": "The score of the alpha team",
-                  "$ref": "#/$defs/clan-battle-team"
-                },
-                "bravo": {
-                  "description": "The score of the alpha team",
-                  "$ref": "#/$defs/clan-battle-team"
-                }
-              },
-              "required": ["date", "alpha", "bravo"]
-            }
-          }
-        },
-        "required": ["season", "score"]
+  $jsonSchema: {
+    title: 'Clan\'s schema',
+    description: 'Clan\'s data',
+    bsonType: 'object',
+    required: [
+      '_id',
+      'id',
+      'name',
+      'tag'
+    ],
+    additionalProperties: false,
+    properties: {
+      _id: {
+        description: 'Mongo\'s identifier',
+        bsonType: 'objectId'
+      },
+      id: {
+        description: 'Clan\'s identifier',
+        bsonType: 'string'
+      },
+      name: {
+        description: 'Clan\'s name',
+        bsonType: 'string'
+      },
+      tag: {
+        description: 'Clan\'s tag',
+        bsonType: 'string'
+      },
+      representations: {
+        description: 'Discord\'s identifiers of the clan\'s representations',
+        bsonType: 'array',
+        minItems: 0,
+        maxItems: 2,
+        items: {
+          bsonType: 'string'
+        }
       }
     }
-  },
-  "required": ["_id", "id", "name", "tag"],
-  "$defs": {
-    "clan-battle-team": {
-      "type": "object",
-      "properties": {
-        "battle": {
-          "description": "The number of the battles played",
-          "type": "number",
-          "minimum": 0
-        }, 
-        "win rate": {
-          "description": "The win rate of the alpha team",
-          "type": "number",
-          "minimum": 0, 
-          "maximum": 100
-        },
-        "league": {
-          "description": "The league of the alpha team",
-          "type": "string"
-        },
-        "division": {
-          "description": "The division of the alpha team",
-          "type": "number",
-          "minimum": 1,
-          "maximum": 3
-        },
-        "score": {
-          "description": "The score of the alpha team",
-          "type": "number",
-          "minimum": 0,
-          "maximum": 100
-        },
-        "promotion": {
-          "description": "The promotion battle result",
-          "type": "array",
-          "minimum": 0,
-          "maximum": 5,
-          "items": {
-            "type": "boolean"
-          }
+  }
+}
+```
+
+### Collection `rank`
+```json lines
+{
+  $jsonSchema: {
+    title: 'Clan\'s Rank',
+    description: 'The clan\'s rank of the clan battle',
+    bsonType: 'object',
+    required: [
+      '_id',
+      'clan',
+      'season',
+      'day',
+      'date',
+      'squad',
+      'battle',
+      'winrate',
+      'league',
+      'division',
+      'score',
+      'raw'
+    ],
+    additionalProperties: false,
+    properties: {
+      _id: {
+        description: 'The clan\'s rank identifier',
+        bsonType: 'objectId'
+      },
+      clan: {
+        description: 'The clan\'s identifier',
+        bsonType: 'string'
+      },
+      season: {
+        description: 'The season of the clan battle',
+        bsonType: 'int'
+      },
+      day: {
+        description: 'The progressive day of the clan battle',
+        bsonType: 'int'
+      },
+      date: {
+        description: 'The date of the clan battle',
+        bsonType: 'string'
+      },
+      squad: {
+        description: 'It states if the squad is alpha',
+        bsonType: 'bool'
+      },
+      battle: {
+        description: 'The number of the battles played',
+        bsonType: 'int',
+        minimum: 0
+      },
+      winrate: {
+        description: 'The win rate of the alpha team',
+        bsonType: 'double',
+        minimum: 0,
+        maximum: 100
+      },
+      league: {
+        description: 'The league of the alpha team',
+        bsonType: 'string'
+      },
+      division: {
+        description: 'The division of the alpha team',
+        bsonType: 'int',
+        minimum: 1,
+        maximum: 3
+      },
+      score: {
+        description: 'The score of the alpha team',
+        bsonType: 'int',
+        minimum: 0,
+        maximum: 100
+      },
+      raw: {
+        description: 'The clan global rank',
+        bsonType: 'double'
+      },
+      promotion: {
+        description: 'The promotion battle result',
+        bsonType: 'array',
+        minimum: 0,
+        maximum: 5,
+        items: {
+          bsonType: 'bool'
         }
       }
     }
@@ -125,36 +142,39 @@ A simple version of MongoDB api for WoWsItalia.
 ```
 
 ### Collection `players`
-```json
+```json lines
 {
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "$id": "schemas/players/detail",
-  "title": "Player Data",
-  "description": "This document records the details of a player",
-  "type": "object",
-  "properties": {
-    "_id": {
-      "description": "A unique identifier for MongoDB (ObjectId)",
-      "type": "string"
-    },
-    "discord": {
-      "description": "The Discord's identifier of the player",
-      "type": "number"
-    },
-    "wows": {
-      "description": "The WoWs' identifier of the player",
-      "type": "number"
-    },
-    "token": {
-      "description": "The WoWs' token of the player",
-      "type": "string"
-    },
-    "expire": {
-      "description": "The expiration day of the WoWs' token in UNIX time",
-      "type": "number"
+  $jsonSchema: {
+    title: 'Player\'s schema',
+    description: 'Player\'s personal data',
+    bsonType: 'object',
+    required: [
+      '_id',
+      'discord',
+      'wows'
+    ],
+    properties: {
+      _id: {
+        description: 'Mongo\'s identifier',
+        bsonType: 'objectId'
+      },
+      discord: {
+        description: 'Discord\'s identifier',
+        bsonType: 'string'
+      },
+      wows: {
+        description: 'WoWs\' identifier',
+        bsonType: 'string'
+      },
+      token: {
+        description: 'WoWs\' token',
+        bsonType: 'string'
+      },
+      expire: {
+        description: 'Expiration date of WoWs\' token',
+        bsonType: 'string'
+      }
     }
-  },
-  "required": ["_id", "id", "discord", "wows"]
+  }
 }
 ```
-
--- api/mongo/api.py
+++ api/mongo/api.py
@@ -16,7 +16,7 @@ class ApiMongoDB:
     #                                                PRIVATE API                                                       #
     ####################################################################################################################
     # Parametric API to mongo
-    def __get_element(self, collection: DBCollections, query: dict) -> any | None:
+    def __get_element(self, collection: DBCollections, query: dict) -> any:
         """
         Gets an element which matches with `query` from a collection of the database.
 
@@ -26,7 +26,7 @@ class ApiMongoDB:
 
         Returns:
             The result of the function `find_one`.
-            If an error occurs, it returns `None`.
+             If an error occurs, it returns `None`.
         """
         try:
             return self.client[self.database_name][str(collection)].find_one(query)
@@ -62,7 +62,7 @@ class ApiMongoDB:
 
         Returns:
             The result of the function `insert_one`.
-            If an error occurs, it returns `None`.
+             If an error occurs, it returns `None`.
         """
         try:
             return self.client[self.database_name][str(collection)].insert_one(document)
@@ -82,7 +82,7 @@ class ApiMongoDB:
 
         Returns:
             The result of the function `update_one`.
-            If an error occurs, it returns `None`.
+             If an error occurs, it returns `None`.
         """
         try:
             return self.client[self.database_name][str(collection)].update_one(query, {"$set": new_data})
@@ -101,7 +101,7 @@ class ApiMongoDB:
 
         Returns:
             The result of the function `delete_one`.
-            If an error occurs, it returns `None`.
+             If an error occurs, it returns `None`.
         """
         try:
             return self.client[self.database_name][str(collection)].delete_one(query)
@@ -111,15 +111,15 @@ class ApiMongoDB:
 
     def __get_clan(self, key: str, value: str) -> dict:
         """
-        Returns the clan data of the clan that `key` matches with `value`.
+        Returns the clan's data of the clan that its `key` matches with `value`.
 
         Args:
-            key: the dictionary's key.
+            key: The dictionary's key.
             value: The key's value.
 
         Returns:
-            The clan data.
-            If an error occurs, it returns an empty dictionary.
+            The clan's data.
+             If an error occurs or if no clan is found, it returns an empty dictionary.
         """
         result = self.__get_element(DBCollections.CLANS, {key: value})
         if result:
@@ -127,9 +127,9 @@ class ApiMongoDB:
         else:
             return {}
 
-    def get_clans(self, key: str, value: str) -> list:
+    def __get_clans(self, key: str, value: str) -> list:
         """
-        Return the clans' data of the clans that the value of `key` contains `value`.
+        Returns the clan's data of the clans that its `key` contains `value`.
 
         Args:
             key: The dictionary's key.
@@ -137,13 +137,30 @@ class ApiMongoDB:
 
         Returns:
             A list of clans.
-            If no clan was found, it returns an empty list.
+             If no clan is found, it returns an empty list.
         """
         result = self.__get_elements(DBCollections.CLANS, {key: {'$regex': value, '$options': 'i'}})
         if result:
             return list(result)
         else:
             return []
+
+    def __get_player(self, query: dict) -> dict:
+        """
+        Returns the player's data that match with `query`.
+
+        Args:
+            query: The query.
+
+        Returns:
+            The player data.
+             If an error occurs or if no player is found, it returns an empty dictionary.
+        """
+        player = self.__get_element(DBCollections.PLAYERS, query)
+        if player:
+            return player
+        else:
+            return {}
 
     ####################################################################################################################
     #                                                 PUBLIC API                                                       #
@@ -170,7 +187,7 @@ class ApiMongoDB:
 
         Returns:
             The updated result.
-            If an error occurs, it returns `None`.
+             If an error occurs, it returns `None`.
         """
         return self.__update_element(DBCollections.CONFIG, {"_id": ObjectId(get_config_id())}, {"$set": config_data})
 
@@ -179,213 +196,257 @@ class ApiMongoDB:
     ####################################################################################################################
     def get_clan_by_id(self, clan_id: str) -> dict:
         """
-        Returns the clan data of the clan that clan's id matches with `clan_id`.
+        Returns the clan's data of the clan that its id matches with `clan_id`.
 
         Args:
-            clan_id: The clan id.
+            clan_id: The clan's id.
 
         Returns:
-            The clan data.
-            If an error occurs, it returns an empty dictionary.
+            The clan's data.
+             If an error occurs, it returns an empty dictionary.
         """
         return self.__get_clan("id", clan_id)
 
     def get_clan_by_tag(self, clan_tag: str) -> dict:
         """
-        Returns the clan data of the clan that clan's tag matches with `clan_tag`.
+        Returns the clan's data of the clan that its tag matches with `clan_tag`.
 
         Args:
-            clan_tag: The clan tag.
+            clan_tag: The clan's tag.
 
         Returns:
-            The clan data.
-            If an error occurs, it returns an empty dictionary.
+            The clan's data.
+             If an error occurs, it returns an empty dictionary.
         """
         return self.__get_clan("tag", clan_tag)
 
     def get_clan_by_name(self, clan_name: str) -> dict:
         """
-        Returns the clan data of the clan that clan's name matches with `clan_name`.
+        Returns the clan's data of the clan that its name matches with `clan_name`.
 
         Args:
-            clan_name: The clan name.
+            clan_name: The clan's name.
 
         Returns:
-            The clan data.
-            If an error occurs, it returns an empty dictionary.
+            The clan's data.
+             If an error occurs, it returns an empty dictionary.
         """
         return self.__get_clan("name", clan_name)
 
     def get_clans_by_id(self, clan_id: str) -> list:
         """
-        Returns the clans' data of the clans that id contains `clan_id`.
+        Returns the clan's data of the clans that their id contains `clan_id`.
 
         Args:
-            clan_id: The clan id.
+            clan_id: The clan's id.
 
         Returns:
             A list of clans.
-            If no clan was found, it returns an empty list.
+             If no clan is found, it returns an empty list.
         """
-        return self.get_clans("id", clan_id)
+        return self.__get_clans("id", clan_id)
 
     def get_clans_by_tag(self, clan_tag: str) -> list:
         """
-        Returns the clans' data of the clans that tag contains `clan_id`.
+        Returns the clan's data of the clans that their tag contains `clan_tag`.
 
         Args:
-            clan_tag: The clan tag.
+            clan_tag: The clan's tag.
 
         Returns:
             A list of clans.
-            If no clan was found, it returns an empty list.
+             If no clan is found, it returns an empty list.
         """
-        return self.get_clans("tag", clan_tag)
+        return self.__get_clans("tag", clan_tag)
 
     def get_clans_by_name(self, clan_name: str) -> list:
         """
-        Returns the clans' data of the clans that name contains `clan_name`.
+        Returns the clan's data of the clans that name contains `clan_name`.
 
         Args:
             clan_name: The clan name.
 
         Returns:
             A list of clans.
-            If no clan was found, it returns an empty list.
+             If no clan is found, it returns an empty list.
         """
-        return self.get_clans("name", clan_name)
+        return self.__get_clans("name", clan_name)
 
     def insert_clan(self, clan_info: dict) -> results.InsertOneResult | None:
         """
         Inserts a clan in the collection.
 
         Args:
-            clan_info: The data of the clan. It has an "id" (str), a "tag" (str), a "name" (str) and a "representations"
-             (list[str]). "representations" is a list of 2 strings.
+            clan_info: The data of the clan.
 
         Returns:
-            the result of the function "__insert_element".
+            The result of the function `__insert_element`.
+             If an error occurs, or if the clan is already stored, it returns `None`.
         """
-        if self.get_clan_by_id(str(clan_info['id'])) is not None:
-            # The clan is already stored
-            return None
-        r1 = None
-        r2 = None
-        try:
-            r1 = str(clan_info['representations'][0])
-            r2 = str(clan_info['representations'][1])
-        except Exception as error:
-            print(error)
-            pass
-        return self.__insert_element(
-            DBCollections.CLANS,
-            {
-                'id': str(clan_info['id']),
-                'name': str(clan_info['name']),
-                'tag': str(clan_info['tag']),
-                'representations': [
-                    r1,
-                    r2
-                ]
-            })
+        if not self.get_clan_by_id(str(clan_info["id"])):
+            representations = []
+            try:
+                representations.append(str(clan_info["representations"][0]))
+            except (KeyError, IndexError):
+                pass
+            try:
+                representations.append(str(clan_info["representations"][1]))
+            except (KeyError, IndexError):
+                pass
+            return self.__insert_element(
+                DBCollections.CLANS,
+                {
+                    "id": str(clan_info["id"]),
+                    "name": str(clan_info["name"]),
+                    "tag": str(clan_info["tag"]),
+                    "representations": representations
+                })
 
-    def update_clan(self, clan_id: str, clan_data: dict) -> results.UpdateResult | None:
+    def update_clan_by_id(self, clan_id: str, clan_data: dict) -> results.UpdateResult | None:
         """
-        Update the clan data. The clan to update is the clan that player_id matches with `clan_id`.
+        Updates the clan's data.
+        The clan to update is the clan that its id matches with `clan_id`.
 
         Args:
-            clan_id: the clan player_id.
-            clan_data: the clan data to insert. If a data exists, it updates the old data.
+            clan_id: The clan's id.
+            clan_data: The clan's data to update. If a data doesn't exist, it inserts the new data.
 
         Returns:
-            the result of the function "__update_element".
+            The result of the function `__update_element`.
+             If an error occurs, it returns `None`.
         """
-        return self.__update_element(DBCollections.CLANS, {'id': clan_id}, clan_data)
+        return self.__update_element(DBCollections.CLANS, {"id": clan_id}, clan_data)
 
-    def delete_clan(self, clan_id: str) -> results.DeleteResult | None:
+    def delete_clan_by_id(self, clan_id: str) -> results.DeleteResult | None:
         """
-        Delete a clan from the collection. The clan to delete is the clan that player_id matches with `clan_id`.
+        Deletes a clan from the collection.
+        The clan to delete is the clan that its id matches with `clan_id`.
 
         Args:
-            clan_id: the clan player_id.
+            clan_id: The clan's id.
 
         Returns:
-            the result of the function "__delete_element".
+            The result of the function `__delete_element`.
         """
-        return self.__delete_element(DBCollections.CLANS, {'id': clan_id})
+        return self.__delete_element(DBCollections.CLANS, {"id": clan_id})
 
-    # Player Collection
-    def get_player(self, discord_id: str = "", wows_id: str = "") -> dict | None:
+    ####################################################################################################################
+    #                                             Player Collection API                                                #
+    ####################################################################################################################
+    def get_player(self, discord_id: str, wows_id: str) -> dict:
         """
-        Returns the player's data that discord's id or wows' id matches with `discord_id` or `wows_id`.
-        If nothing is matched, it returns `None`.
+        Returns the player's data that Discord's id and WoWs' id matches with `discord_id` and `wows_id`.
 
         Args:
-            discord_id: the discord's id.
-            wows_id: the wows' id.
+            discord_id: the Discord's id.
+            wows_id: the WoWs' id.
 
         Returns:
-            the result of the function "__delete_element".
+            The player data.
+             If no player is found, it returns an empty dictionary.
         """
-        query = {
-            '$or': [
-                {'discord': discord_id},
-                {'wows': wows_id}
-            ]
-        }
-        x = self.__get_elements(DBCollections.PLAYERS, query)
-        if x:
-            return list(x)[0]
+        if discord_id and wows_id:
+            return self.__get_player({"$and": [
+                {"discord": discord_id},
+                {"wows": wows_id}
+            ]})
         else:
-            return None
+            return {}
 
-    def insert_player(self, discord_id: str, wows_id: str, token: str, expire: str) -> results.InsertOneResult | None:
+    def get_player_by_discord(self, discord_id: str) -> dict:
         """
-        Search and return a player that discord's id or wows' id matches with the arguments.
-        If nothing is matched, it returns `None`.
+        Returns the player's data that Discord's id matches with `discord_id`.
 
         Args:
-            discord_id: the discord's id.
-            wows_id: the wows' id.
+            discord_id: the Discord's id.
 
         Returns:
-            the result of the function "__delete_element".
+            The player data.
+             If no player is found, it returns an empty dictionary.
+        """
+        return self.__get_player({"discord": discord_id})
+
+    def get_player_by_wows(self, wows_id: str) -> dict:
+        """
+        Returns the player's data that WoWs' id matches with `wows_id`.
+
+        Args:
+            wows_id: the WoWs' id.
+
+        Returns:
+            The player data.
+             If no player is found, it returns an empty dictionary.
+        """
+        return self.__get_player({"wows": wows_id})
+
+    def insert_player(
+            self,
+            discord_id: str,
+            wows_id: str,
+            token: str = "",
+            expire: str = ""
+    ) -> results.InsertOneResult | None:
+        """
+        Inserts a player in the collection.
+
+        Args:
+            discord_id: The Discord's id of the player.
+            wows_id: The WoWs' id of the player.
+            token: The token to access the personal data of the wargaming API.
+            expire: The date of the token's expire.
+
+        Returns:
+            The result of the function `__insert_element`.
+            If an error occurs, or if the player is already stored, it returns `None`.
         """
         player = self.get_player(discord_id, wows_id)
-        if player:
-            if player["discord_id"] == discord_id:
-                print("Discord ID is already in the DB.")
-            elif player["wows_id"] == wows_id:
-                print("WoWs ID is already in the DB.")
-            else:
-                print("sus: this should never happens")
-            return None
-        else:
+        if not player:
             inserted_player = self.__insert_element(
                 DBCollections.PLAYERS,
                 {
-                    'discord': discord_id,
-                    'wows': wows_id,
-                    'token': token,
-                    'expire': expire
+                    "discord": discord_id,
+                    "wows": wows_id,
+                    "token": token,
+                    "expire": expire
                 }
             )
             if inserted_player:
                 if inserted_player.inserted_id:
                     return inserted_player
-            return None
 
-    def delete_player(
-        self,
-        discord_id: str = "",
-        wows_id: str = "",
-    ) -> results.DeleteResult | None:
+    def update_player(self, discord_id: str, player_data: dict) -> results.UpdateResult | None:
+        """
+        Updates the player's data.
+        The player to update is the player that its Discord's id matches with `discord_id`.
+
+        Args:
+            discord_id: The Discord's id of the player.
+            player_data: The player's data to update. If a data doesn't exist, it inserts the new data.
+
+        Returns:
+            The result of the function `__update_element`.
+             If an error occurs, it returns `None`.
+        """
+        return self.__update_element(DBCollections.PLAYERS, {"discord": discord_id}, player_data)
+
+    def delete_player(self, discord_id: str = "", wows_id: str = "") -> results.DeleteResult | None:
+        """
+        Deletes a player from the collection.
+        The player to delete is the player that its Discord's and WoWs' id matches with `discord_id` and `clan_id`.
+
+        Args:
+            discord_id: The Discord's id of the player.
+            wows_id: The WoWs' id of the player.
+
+        Returns:
+            The result of the function `__delete_element`.
+        """
         return self.__delete_element(
             DBCollections.PLAYERS,
-            {
-                '$or': [
-                    {'discord': discord_id},
-                    {'wows': wows_id}
-                ]
-            }
+            {'$and': [
+                {'discord': discord_id},
+                {'wows': wows_id}
+            ]}
         )
+
+    # TODO: Change return value of update (update nothing, insert new data) and delete (delete nothing)
